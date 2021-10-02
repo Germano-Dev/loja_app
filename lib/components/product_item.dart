cb038fcdbde7da3bd722e91d1342586f5ac1c52c
Germano-Dev <88905779+Germano-Dev@users.noreply.github.com>
2021-10-02 18:42:41 -0300

    commit 03
    Continuação da tela de produtos

---
+++ lib/components/product_item.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:loja_app/models/product.dart';
+
+class ProductItem extends StatelessWidget {
+  final Product product;
+  const ProductItem({
+    Key? key,
+    required this.product,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: Image.network(
+          product.imageUrl,
+          fit: BoxFit.cover,
+        ),
+        footer: GridTileBar(
+          title: Text(
+            product.title,
+            textAlign: TextAlign.center,
+          ),
+          leading: IconButton(
+            icon: Icon(Icons.favorite),
+            onPressed: () {},
+          ),
+          backgroundColor: Colors.black45,
+          trailing: IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.shopping_cart),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/screen/products_overview.dart
+++ lib/screen/products_overview.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:loja_app/components/product_item.dart';
 import 'package:loja_app/data/dummy_data.dart';
 
 import 'package:loja_app/models/product.dart';
@@ -13,17 +14,15 @@ class ProductsOverviewPages extends StatelessWidget {
       appBar: AppBar(
         title: Text('Loja App'),
       ),
-      body: Padding(
+      body: GridView.builder(
         padding: const EdgeInsets.all(10),
-        child: GridView.builder(
-          itemCount: loadedProducts.length,
-          itemBuilder: (ctx, i) => Text(loadedProducts[i].title),
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2, // QUANTIDADE DE COLUNAS
-            childAspectRatio: 3/2, // TAMANHO DA COLUNA
-            crossAxisSpacing: 10, // ESPAÇAMENTO VERTICAL 
-            mainAxisSpacing: 10, //ESPAÇAMENTO HORIZONTAL
-           ),
+        itemCount: loadedProducts.length,
+        itemBuilder: (ctx, i) => ProductItem(product: loadedProducts[i]),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2, // QUANTIDADE DE COLUNAS
+          childAspectRatio: 3 / 2, // TAMANHO DA COLUNA
+          crossAxisSpacing: 10, // ESPAÇAMENTO VERTICAL
+          mainAxisSpacing: 10, //ESPAÇAMENTO HORIZONTAL
         ),
       ),
     );
